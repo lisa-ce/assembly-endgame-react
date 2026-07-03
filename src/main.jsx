c54e9f0b04f82707fe801c40484e2768eba12a50
Lisa Chikovore <lisachikovore@gmail.com>
2026-07-03 19:45:23 +0200

    added the game header

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import AssemblyEndgame from "./App"
+import "./index.css"
 
-createRoot(document.getElementById('root')).render(
-
-    <App />
-
-)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AssemblyEndgame/>)
